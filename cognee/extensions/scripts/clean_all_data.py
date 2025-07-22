473a082c172daf4536902629905c797a32ccb5ff
zhaozhonghua <zhonghua.zhao@sprounix.com>
2025-07-22 16:35:12 +0800

    fix clearn_all_data

--- cognee/extensions/scripts/clean_all_data.py
+++ cognee/extensions/scripts/clean_all_data.py
@@ -7,7 +7,7 @@ async def clearn_all_data():
     # nest_asyncio.apply()
     print("Deleting all files and data...")
     await cognee.prune.prune_data()
-    await cognee.prune.prune_system(metadata=False)
+    await cognee.prune.prune_system(metadata=True)
     print("All files deleted.")
 
 
